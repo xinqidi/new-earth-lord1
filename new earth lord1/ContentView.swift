5634baa2d9ed1dcd2b81bb031decf5e73e9d8645
nanjifangke <a0935309320@gmail.com>
2025-12-23 20:25:04 +0800

    Initial Commit

---
+++ new earth lord1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  new earth lord1
+//
+//  Created by 新起点 on 2025/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ new earth lord1/new_earth_lord1App.swift
@@ -0,0 +1,17 @@
+//
+//  new_earth_lord1App.swift
+//  new earth lord1
+//
+//  Created by 新起点 on 2025/12/23.
+//
+
+import SwiftUI
+
+@main
+struct new_earth_lord1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
